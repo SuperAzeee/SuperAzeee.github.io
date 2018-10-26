2ca218dcfe0f8431189894d487e4e5450cc40d35
SuperAzeee <SuperAzeee123@gmail.com>
2018-10-26 14:23:02 +0200

    Testing phaser
    26/10/18

---
+++ Test1.js
@@ -0,0 +1,18 @@
+class Test1 extends Phaser.Scene {
+    constructor(){
+        super({key:"Test1"})
+    }
+
+    preload(){
+        this.load.image('Title','assets/Title.png');
+
+
+    }
+
+
+    create(){
+
+        this.image = this.add.image(400,300,'Title');
+    }
+
+}
---
+++ game.js
@@ -0,0 +1,14 @@
+
+
+var config ={
+
+    type:Phaser.AUTO,
+    width:800,
+    height:600,
+
+    scene: [Test1]
+};
+
+var game = new Phaser.Game(config);
+
+
